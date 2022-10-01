f08b96d173a27ad4a53d213e58946dec4ce2b7fe
TselmuunUranchimeg <tselmuunu@gmail.com>
2022-10-02 01:56:51 +0800

    Fixed bug correlating with inputs length and slow update

--- components/form/form.tsx
+++ components/form/form.tsx
@@ -1,20 +1,18 @@
 import {
-    ChangeEvent,
+    useState,
+    useRef,
     FormEvent,
     Dispatch,
     SetStateAction,
-    useState,
 } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
 import TextField from "@mui/material/TextField";
-import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import Snackbar from "@mui/material/Snackbar";
-import { Exercise, RoutineBody } from "../../types/general.types";
 import Slide, { SlideProps } from "@mui/material/Slide";
+import { Exercise, RoutineBody } from "../../types/general.types";
 import styles from "../../styles/WorkoutForm.module.css";
+import Input from "./input";
 
 type WorkoutFormType = {
     callbackUrl: string;
@@ -43,23 +41,49 @@ const WorkoutForm = ({
     const router = useRouter();
     const [open, setOpen] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
+    const divElement = useRef<null | HTMLDivElement>(null);
 
-    const inputChange = async (
-        e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-        ind: number,
-        isReps: boolean
-    ) => {
-        setInputFields((prev) => {
-            let newArr = [...prev];
-            let target = newArr[ind];
-            if (isReps) {
-                target.requirement = { reps: parseInt(e.target.value) };
-            } else {
-                target.requirement = parseInt(e.target.value);
-            }
-            return newArr;
-        });
+    const updateExercise= async (index: number, value: Exercise) => {
+        inputs[index] = value;
     };
+
+    const addInputField = async () => {
+        try {
+            await (async () => {
+                setInputFields(prev => {
+                    return [...prev, {
+                        exerciseName: "",
+                        requirement: 0
+                    }];
+                });
+            })();
+            divElement.current!.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
+    const removeInputField = async (ind: number) => {
+        if (inputs.length > 1) {
+            setInputFields(prev => {
+                let newArr = prev.filter((_, i) => i !== ind);
+                return newArr;
+            });
+        }
+    }
+
+    const copyInputField = async (ind: number) => {
+        setInputFields(prev => {
+            return [
+                ...prev.slice(0, ind + 1),
+                prev[ind],
+                ...prev.slice(ind+1)
+            ];
+        });
+    }
 
     const submit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -70,7 +94,7 @@ const WorkoutForm = ({
                 (typeof r === "object" && r.reps === 0) ||
                 (typeof r === "number" && r === 0)
             ) {
-                emptyFields.push(ind+1);
+                emptyFields.push(ind + 1);
             }
         });
         if (emptyFields.length === 0) {
@@ -78,7 +102,6 @@ const WorkoutForm = ({
                 name,
                 exercises: inputs,
             };
-            console.log(postBody);
             let res = await axios({
                 method,
                 data: postBody,
@@ -122,145 +145,19 @@ const WorkoutForm = ({
             <ol>
                 {inputs.map((val, ind) => {
                     return (
-                        <li key={ind} className="relative">
-                            <div className="flex mobileL:w-[calc(100%-48px)] sm:items-center justify-between box-border pr-[10px] mobileL:items-end items-start mobileL:flex-row flex-col w-100%">
-                                <div className="flex sm:flex-row flex-col items-center">
-                                    <div className="flex sm:flex-row sm:items-center items-end">
-                                        <h1 className="mt-3 mr-3 text-xl">
-                                            {ind + 1}.{" "}
-                                        </h1>
-                                        <div className="flex sm:flex-row flex-col">
-                                            <TextField
-                                                required
-                                                type="text"
-                                                value={val.exerciseName}
-                                                onChange={(e) => {
-                                                    setInputFields((prev) => {
-                                                        let newArr = [...prev];
-                                                        let target =
-                                                            newArr[ind];
-                                                        target.exerciseName =
-                                                            e.target.value;
-                                                        return newArr;
-                                                    });
-                                                }}
-                                                label="Exercise name"
-                                                InputLabelProps={{
-                                                    className: "!text-white",
-                                                }}
-                                                InputProps={{
-                                                    className: "!text-white",
-                                                }}
-                                                className="!pr-3 sm:!mb-0 !mt-3"
-                                            />
-                                            <TextField
-                                                required
-                                                type="number"
-                                                value={
-                                                    typeof val.requirement ===
-                                                    "object"
-                                                        ? 0
-                                                        : val.requirement
-                                                }
-                                                onChange={async (e) =>
-                                                    await inputChange(
-                                                        e,
-                                                        ind,
-                                                        false
-                                                    )
-                                                }
-                                                label="Duration"
-                                                InputLabelProps={{
-                                                    className: "!text-white",
-                                                }}
-                                                InputProps={{
-                                                    className: "!text-white",
-                                                    inputProps: {
-                                                        min: 0,
-                                                    },
-                                                }}
-                                                className="!pr-3 sm:!mb-0 !mt-3"
-                                            />
-                                            <TextField
-                                                required
-                                                type="number"
-                                                value={
-                                                    typeof val.requirement ===
-                                                    "object"
-                                                        ? val.requirement.reps
-                                                        : 0
-                                                }
-                                                onChange={async (e) =>
-                                                    await inputChange(
-                                                        e,
-                                                        ind,
-                                                        true
-                                                    )
-                                                }
-                                                label="Reps"
-                                                InputLabelProps={{
-                                                    className: "!text-white",
-                                                }}
-                                                InputProps={{
-                                                    className: "!text-white",
-                                                    inputProps: {
-                                                        min: 0,
-                                                    },
-                                                }}
-                                                className="!pr-3 sm:!mb-0 !mt-3"
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="flex mobileL:mt-0 mobileL:ml-0 mt-3 ml-7">
-                                    <RemoveIcon
-                                        onClick={() => {
-                                            setInputFields((prev) => {
-                                                if (prev.length > 1) {
-                                                    prev = prev.filter(
-                                                        (_, i) => i !== ind
-                                                    );
-                                                }
-                                                return prev;
-                                            });
-                                        }}
-                                        className={`${styles.workoutIcon} mr-2`}
-                                    />
-                                    <ContentCopyIcon
-                                        className={`${styles.workoutIcon} mr-2`}
-                                        onClick={() => {
-                                            setInputFields((prev) => {
-                                                let newArr = [
-                                                    ...prev.slice(0, ind + 1),
-                                                    prev[ind],
-                                                    ...prev.slice(ind + 1),
-                                                ];
-                                                return newArr;
-                                            });
-                                        }}
-                                    />
-                                </div>
-                            </div>
-                            <AddIcon
-                                className={`${styles.workoutIcon} ${
-                                    inputs.length - 1 === ind
-                                        ? "!inline-block"
-                                        : "!hidden"
-                                } absolute bottom-0 right-0`}
-                                onClick={() => {
-                                    setInputFields((prev) => {
-                                        let newArr = [...prev];
-                                        newArr.push({
-                                            exerciseName: "",
-                                            requirement: 0,
-                                        });
-                                        return newArr;
-                                    });
-                                }}
-                            />
-                        </li>
+                        <Input 
+                            key = {1000-ind}
+                            val = {val}
+                            ind = {ind}
+                            inputs = {inputs}
+                            updateExercise = {updateExercise}
+                            addInputField = {addInputField}
+                            copyInputField = {copyInputField}
+                            removeInputField = {removeInputField}
+                        />
                     );
                 })}
+                <div ref={divElement}></div>
             </ol>
             <div className="flex mobileM:flex-row flex-col">
                 <button
